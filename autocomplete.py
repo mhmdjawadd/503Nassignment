d21f6a45db217ec2c41351338535cc453234284d
reem-chaaban <rrc12@mail.aub.edu>
2025-04-06 21:38:18 +0300

    Slightly modified autocomplete.py

--- autocomplete.py
+++ autocomplete.py
@@ -3,6 +3,9 @@ from transformers import pipeline
 fill_mask = pipeline('fill-mask', model = 'bert-base-uncased')
 
 #takes sentence w/ [MASK] token & returns completed sentence
+#argument: sentence (str) = takes input sentence including the [MASK] token
+#returns: str = sentence / [MASK] replaced by the model's top (best) prediction
+
 def autocomplete_sentence(sentence: str) -> str: 
     if "[MASK]" not in sentence:
         raise ValueError("The input sentence must contain a [MASK] token!")
@@ -10,8 +13,8 @@ def autocomplete_sentence(sentence: str) -> str:
     try:
         predictions = fill_mask(sentence)
         if predictions:
-            top_prediction = predictions[0]['token_str']
-            complete_sentence = sentence.replace("[MASK]", top_prediction)
+            top_prediction = predictions[0]['token_str'].strip()
+            complete_sentence = sentence.replace("[MASK]", top_prediction, 1)
             return complete_sentence
         else:
             return "Prediction can't be generated."
